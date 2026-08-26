32cba5ed8815e22c79dc664febbedd788b6b37d5
Ivo Zoppi <zoppiivo@gmail.com>
2026-08-26 11:36:20 +0200

    Mappatura RichiestaBoundary
    Mappatura RichiestaBoundary

--- progetto-ids/src/main/java/unicam/ids/hackhub/controller/RichiestaBoundary.java
+++ progetto-ids/src/main/java/unicam/ids/hackhub/controller/RichiestaBoundary.java
@@ -4,15 +4,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import unicam.ids.hackhub.dto.RichiestaDTO;
 import unicam.ids.hackhub.service.GestoreRichiesta;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/richieste")
 public class RichiestaBoundary {
     private final GestoreRichiesta gestoreRichiesta;
 
